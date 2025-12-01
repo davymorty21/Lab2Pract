7629b6b073468691337c3026f1ba58e29bf1394f
HanSitt <hanhsl02.24@ichat.sp.edu.sg>
2025-12-01 19:26:18 +0800

    Showing Weight Statndard

---
+++ bmi.py
@@ -0,0 +1,15 @@
+def cal_bmi(height,weight):
+    print("height :" + height)
+    print("weight :" + weight)
+    bmi = float(weight)/float(height)**2
+    result = round (bmi, 2)
+    print("BMI", result)
+
+    if bmi < 18.5:
+        print("Underweight")
+    elif bmi >= 18.5 and bmi < 25.0:
+        print("noraml weight")
+    else:
+        print("overweight")
+
+cal_bmi(height ="1.73", weight= "57")
